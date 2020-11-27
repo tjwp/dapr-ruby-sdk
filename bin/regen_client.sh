ab912b1b8137cb7fcd74d228f5dfb19051a9cdd5
tjwp <tjwp@users.noreply.github.com>
2020-11-27 08:19:15 -0500

    Update regen script to prefix Dapr top-level constant

--- bin/regen_client.sh
+++ bin/regen_client.sh
@@ -11,5 +11,11 @@ bundle exec grpc_tools_ruby_protoc -I . --ruby_out=lib --grpc_out=lib dapr/proto
 bundle exec grpc_tools_ruby_protoc -I . --ruby_out=lib --grpc_out=lib dapr/proto/runtime/v1/dapr.proto
 bundle exec grpc_tools_ruby_protoc -I . --ruby_out=lib --grpc_out=lib dapr/proto/runtime/v1/appcallback.proto
 
+# Prefix top-level Dapr constant in dapr_services_pb
+dapr_services="lib/dapr/proto/runtime/v1/dapr_services_pb.rb"
+mv $dapr_services ${dapr_services}.tmp
+cat ${dapr_services}.tmp | sed -e "s/ Dapr::Proto/ ::Dapr::Proto/" > $dapr_services
+rm ${dapr_services}.tmp
+
 set +x
 echo "Done."
