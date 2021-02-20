1fed3b7efb43acb9e4344d1c023f75aeb88d4f15
tjwp <tjwp@users.noreply.github.com>
2021-02-20 13:04:12 -0500

    Updated example for v1.0

--- example.rb
+++ example.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
-require "dapr_services_pb"
+require "dapr/proto/runtime/v1/dapr_services_pb"
 
 port = ENV.fetch('DAPR_GRPC_PORT', '5001')
 dapr_uri = "localhost:#{port}"
 
-client = Dapr::Dapr::Stub.new(dapr_uri, :this_channel_is_insecure)
-data = Google::Protobuf::Any.new(value: 'lala')
-client.publish_event(Dapr::PublishEventEnvelope.new(topic: 'sith', data: data))
+client = Dapr::Proto::Runtime::V1::Dapr::Stub.new(dapr_uri, :this_channel_is_insecure)
+data = 'lala'
+client.publish_event(Dapr::Proto::Runtime::V1::PublishEventRequest.new(pubsub_name: 'pubsub', topic: 'sith', data: 'lala'))
 puts('Published')
 
 key = 'mykey'
 store_name = 'statestore'
-req = Dapr::StateRequest.new(key: key, value: Google::Protobuf::Any.new(value: 'my state'))
-state = Dapr::SaveStateEnvelope.new(storeName: store_name, requests: [req])
-client.save_state(state)
+state = Dapr::Proto::Common::V1::StateItem.new(key: key, value: 'my state')
+req = Dapr::Proto::Runtime::V1::SaveStateRequest.new(store_name: store_name, states: [state])
+client.save_state(req)
 puts('Saved!')
 
-resp = client.get_state(Dapr::GetStateEnvelope.new(storeName: store_name, key: key))
+resp = client.get_state(Dapr::Proto::Runtime::V1::GetStateRequest.new(store_name: store_name, key: key))
 puts('Got state!')
 puts(resp)
 
-client.delete_state(Dapr::DeleteStateEnvelope.new(storeName: store_name, key: key))
+client.delete_state(Dapr::Proto::Runtime::V1::DeleteStateRequest.new(store_name: store_name, key: key))
 puts('Deleted!')
